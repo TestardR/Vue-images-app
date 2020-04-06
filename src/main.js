3ab7c603bf6c70f69e119da5622d4d3b331ad796
rtestard <rtestard@opensense.fr>
2020-04-06 20:26:11 +0200

    I have setup basic routing

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router';
 import store from './store'
+
+import App from './App.vue'
+import AuthHandler from './components/AuthHandler'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/oauth2/callback', component: AuthHandler
+    }
+  ]
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   store,
   render: h => h(App),
 }).$mount('#app')
