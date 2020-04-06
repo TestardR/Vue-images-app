49f92c06b9bd284f08662b59d292e30faf1d0c97
rtestard <rtestard@opensense.fr>
2020-04-06 12:57:38 +0200

    I have installed and started Vuex for state management

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <AppHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    AppHeader
   }
-}
+};
 </script>
 
 <style>
